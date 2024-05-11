3f0c991713b899c0ea65de06152c93d64474b6e5
Arzuman Torosian <arzuman.torosian@doublegood.com>
2024-05-11 12:49:25 +0300

    first commit

---
+++ en-to-ru/main.py
@@ -0,0 +1,15 @@
+# Importing library of warnings
+import warnings
+
+import os
+import sys
+
+script_dir = os.path.dirname(__file__)
+transformer_dir = os.path.join(script_dir, '..')
+sys.path.append(transformer_dir)
+import transformer
+
+if __name__ == '__main__':
+    warnings.filterwarnings('ignore') # Filtering warnings
+    config = transformer.get_config(lang_src='en', lang_tgt='ru', seg_len=230) # Retrieving config settings
+    transformer.train_model(config) # Training model with the config arguments
